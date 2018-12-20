a451a0ae5146104b9bdf7211eeeb0621f79aa5e0
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-12-20 14:45:37 +0100

    Absorbed auto.py into app.py

--- vogue/server/app.py
+++ vogue/server/app.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 import coloredlogs
+
+from flask_debugtoolbar import DebugToolbarExtension
 from flask import Flask
+
 from . import extentions
 
 def create_app():
@@ -14,9 +17,20 @@ def create_app():
 
     return app
 
-
 def configure_extensions(app: Flask):
     """Configure extensions and logging"""
     coloredlogs.install(level='DEBUG' if app.debug else 'INFO')
 
     extentions.adapter.init_app(app)
+
+def main():
+    app.debug = True
+    #toolbar = DebugToolbarExtension(app)
+    app.run(host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    main()
+
+app = create_app()
+
+from . import views
