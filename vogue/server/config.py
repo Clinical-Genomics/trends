06f24e84bfb99a54fc6f56687a138df2f4bdc6ac
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-12-20 14:30:27 +0100

    Remove unused config vars

--- vogue/server/config.py
+++ vogue/server/config.py
@@ -1,12 +1,6 @@
 # encoding: utf-8
 import os
 
-# flask
-TEMPLATES_AUTO_RELOAD = True
-
-# server
-CG_ENABLE_ADMIN = ('FLASK_DEBUG' in os.environ)
-
 # mongo
 MONGO_URI = os.environ['MONGO_URI']
 MONGO_DBNAME = os.environ['MONGO_DBNAME']
