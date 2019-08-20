ff10fa359b4323de2251dfa605c0578449e5b1e7
Maya <maya.brandi@scilifelab.se>
2019-08-20 10:03:23 +0200

    sample analysis command no longer used

--- vogue/commands/load/base.py
+++ vogue/commands/load/base.py
@@ -10,7 +10,6 @@ LOG = logging.getLogger(__name__)
 
 # commands
 from vogue.commands.load.bioinfo import bioinfo as bioinfo_command 
-from vogue.commands.load.sample_analysis import sample_analysis as sample_analysis_command
 from vogue.commands.load.application_tag import application_tags as status_db_command
 from vogue.commands.load.flowcell import flowcell as flowcell_command
 from vogue.commands.load.sample import sample as sample_command
