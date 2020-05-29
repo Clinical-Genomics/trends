d211955aa124885a7fc3a4269cfe07b3b2e55a9d
mayabrandi <maya.brandi@scilifelab.se>
2020-05-29 16:02:41 +0200

    Trying to simplify a bit

--- vogue/build/bioinfo_analysis.py
+++ vogue/build/bioinfo_analysis.py
@@ -193,36 +193,26 @@ def update_mongo_doc_case(mongo_doc: dict,
 
     analysis_samples = analysis_dict['sample']
     analysis_case = analysis_dict['case']
-    case_analysis_type = analysis_dict['case_analysis_type']
-    analysis_workflow = analysis_dict['workflow']
+    analysis_type = analysis_dict['case_analysis_type']
+    analysis_workflow_type = analysis_dict['workflow']
     workflow_version = analysis_dict['workflow_version']
 
-    if not processed:
-        # if workflow doesn't exist. e.g. previous workflow was mip, and now loading from balsamic
-        if analysis_workflow in mongo_doc['workflows'] :
-            mongo_doc[analysis_workflow][case_analysis_type] = copy.deepcopy(
-                new_analysis[analysis_workflow][case_analysis_type])
-            mongo_doc['case_analysis_types'] = case_analysis_type
-        else:
-            mongo_doc['workflows'].append(analysis_workflow)
-            mongo_doc[analysis_workflow] = new_analysis[analysis_workflow]
+    
+    if analysis_workflow_type not in mongo_doc:
+        mongo_doc[analysis_workflow_type] = {} 
+
+    if analysis_type not in mongo_doc[analysis_workflow_type]:
+        mongo_doc[analysis_workflow_type][analysis_type] = new_analysis[analysis_workflow_type][analysis_type]
     else:
-        if analysis_workflow in mongo_doc['workflows']:
-            mongo_doc[analysis_workflow] = {
-                **mongo_doc[analysis_workflow],
-                **new_analysis[analysis_workflow]
-            }
-        else:
-            new_workflow_analysis = dict()
-            new_workflow_analysis[case_analysis_type] = new_analysis[analysis_workflow][case_analysis_type]
-            mongo_doc[analysis_workflow] = copy.deepcopy(new_workflow_analysis) 
-            mongo_doc['workflows'].append(analysis_workflow)
+        mongo_doc[analysis_workflow_type] = {**new_analysis[analysis_workflow_type],
+                                             **mongo_doc[analysis_workflow_type]}
+            
+    if analysis_workflow_type not in mongo_doc['workflows']:
+        mongo_doc['workflows'].append(analysis_workflow_type)
 
-        if case_analysis_type not in mongo_doc['case_analysis_types']:
-            LOG.info("A new analysis type %s is added to the case %s", case_analysis_type,
-                     analysis_case)
-            mongo_doc['case_analysis_types'].append(case_analysis_type)
-
+    if analysis_type not in mongo_doc['case_analysis_types']:
+        LOG.info("A new analysis type %s is added to the case %s", analysis_type, analysis_case)
+        mongo_doc['case_analysis_types'].append(analysis_type)
 
     for sample in analysis_samples:
         if sample not in mongo_doc['samples']:
