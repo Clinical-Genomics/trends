04a4891433e0081707df7ee31b72388b5f0f5837
hassanfa <hassan.foroughi@gmail.com>
2021-03-08 12:12:51 +0100

    fine tuned mongodb aggr

--- vogue/server/utils/bioinfo/qc.py
+++ vogue/server/utils/bioinfo/qc.py
@@ -3,16 +3,16 @@
 from vogue.constants.constants import MONTHS, DNA_PICARD
 
 VALID_WORKFLOWS = ["balsamic", "mip-dna"]
+VALID_APPLICATION = ["wgs", "tga", "wes"]
 
 def qc_dna_picard_time_plot(adapter, year: int) -> dict:
     """Prepares data for the picard over time plot."""
 
-    pipe = [
-    {
+    pipe = [{
         '$lookup': {
-            'from': 'sample', 
-            'localField': '_id', 
-            'foreignField': '_id', 
+            'from': 'sample',
+            'localField': '_id',
+            'foreignField': '_id',
             'as': 'sample_info'
         }
     }, {
@@ -27,9 +27,10 @@ def qc_dna_picard_time_plot(adapter, year: int) -> dict:
         }
     }, {
         '$project': {
-            'mip-dna': 1, 
-            'balsamic': 1, 
-            'received_date': '$sample_info.received_date', 
+            'mip-dna': 1,
+            'balsamic': 1,
+            'workflows': 1,
+            'received_date': '$sample_info.received_date',
             'category': '$sample_info.category'
         }
     }, {
@@ -42,22 +43,22 @@ def qc_dna_picard_time_plot(adapter, year: int) -> dict:
         '$project': {
             'month': {
                 '$month': '$received_date'
-            }, 
+            },
             'year': {
                 '$year': '$received_date'
-            }, 
-            'mip-dna': 1, 
-            'balsamic': 1, 
+            },
+            'mip-dna': 1,
+            'balsamic': 1,
+            'workflows': 1,
             'category': 1
         }
     }, {
         '$match': {
             'year': {
-                '$eq': int(year) 
+                '$eq': int(year)
             }
         }
-    }
-]
+    }]
     aggregate_result = adapter.bioinfo_samples_aggregate(pipe)
     final_data = {}
     for data in DNA_PICARD.values():
@@ -67,13 +68,15 @@ def qc_dna_picard_time_plot(adapter, year: int) -> dict:
     for sample in aggregate_result:
         qc_dna_analysis = sample.get('balsamic')
         if not qc_dna_analysis:
-            qc_dna_analysis = sample.get('mip-dna') 
+            qc_dna_analysis = sample.get('mip-dna')
             if not qc_dna_analysis:
                 continue
-        multiqc_picard_insertSize = qc_dna_analysis.get('multiqc_picard_insertSize')
-        multiqc_picard_HsMetrics = qc_dna_analysis.get('multiqc_picard_HsMetrics')
-        final_data = _append_to_final_data(final_data=final_data, raw_data=multiqc_picard_insertSize, sample=sample)
-        #final_data = _append_to_final_data(final_data=final_data, raw_data=multiqc_picard_HsMetrics, sample=sample)
+        qc_data_to_process = qc_dna_analysis.get('multiqc_picard_insertSize')
+        if qc_data_to_process:
+            final_data = _append_to_final_data(
+                final_data=final_data,
+                raw_data=qc_data_to_process,
+                sample=sample)
 
     plot_data = {'data': final_data, 'labels': [m[1] for m in MONTHS]}
     return (plot_data)
@@ -86,6 +89,7 @@ def _append_to_final_data(final_data: dict, raw_data: dict, sample: dict):
         if key in final_data.keys():
             final_data[key].append({
                 'name': sample['_id'],
+                'category': sample['category'],
                 'x': sample['month'],
                 'y': val
             })
@@ -103,8 +107,10 @@ def qc_dna_picard_plot(adapter, year: int) -> dict:
         qc_dna_analysis = sample.get('balsamic')
         if not qc_dna_analysis:
             continue
-        multiqc_picard_insertSize = qc_dna_analysis.get('multiqc_picard_insertSize')
-        multiqc_picard_HsMetrics = qc_dna_analysis.get('multiqc_picard_HsMetrics')
+        multiqc_picard_insertSize = qc_dna_analysis.get(
+            'multiqc_picard_insertSize')
+        multiqc_picard_HsMetrics = qc_dna_analysis.get(
+            'multiqc_picard_HsMetrics')
         merged = multiqc_picard_insertSize.copy()
         merged.update(multiqc_picard_HsMetrics)
         merged['_id'] = sample_id
